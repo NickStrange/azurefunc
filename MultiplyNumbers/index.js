4b41377cc027ef5ad2a7ff063acdbbfdbe740efb
Nick Strange <nsstrange@icloud.com>
2023-05-02 13:55:22 -0400

    local commit

---
+++ MultiplyNumbers/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+    if (req.query.num1 && req.query.num2)
+    {
+        context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: req.query.num1 * req.query.num2
+        }
+    }
+    else{
+        context.res = {
+            status:400,
+            body: "Pass 2 numbers num1 and num2"
+        }
+    }
+}
